aaf3bd869742c76d113b0f6a6ce65d2ca496a673
Younousse Soufiani <soufiani08@live.fr>
2024-10-23 15:30:25 +0200

    supress built files
    add gitignore

--- Programm/autofollow_ws/build/autofollow_lidar_detection/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/younousse/Documents/Autonomouscar/Programm/autofollow_ws/install/autofollow_lidar_detection'
